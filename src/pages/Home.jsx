d7905c333aef953f57ae749e16a8d8d3a9cd3182
[mrudul] <[mrudulkolambe]>
2023-05-21 19:48:38 +0530

    Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 				<div className='custom-height w-screen top-[90px] left-0 absolute flex justify-between px-24'>
 					<div className='h-full w-1/2 flex justify-center flex-col pl-8'>
 						<h1 className='text-6xl font-semibold'><span className='font-extralight'>The largest & trusted</span> Courier Delivery<br/>Service.</h1>
-						<p className='mt-8 text-lg'>We deliver your products safely to your <br /> home in a reasonable time and safely.</p>
+						<p className='mt-8 text-lg'>We deliver your products safely to your <br /> home in a reasonable time and safely..</p>
 						<Button text={"Create a Order"} className={"w-48 py-3 mt-10"}/>
 					</div>
 					<div className='h-full w-1/2 flex items-center'>
